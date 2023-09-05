7fb4b75e57c2fa12111ed8937ae0110f7ede9e65
Rajath H R <rajath.nandan33@gmail.com>
2023-09-05 17:50:01 +0530

    Episode 1

---
+++ App.js
@@ -0,0 +1,22 @@
+{/* <div id='parent'>
+    <div id='child'>
+        <h1>Hello H1</h1>
+        <h2>Hello H2</h2>
+    </div>
+</div> */}
+
+// React create element creates an objects, which while rendering 
+// will convert to html
+const parent = React.createElement('div', {id: 'parent'},
+    React.createElement('div', {id: 'child'},
+    [React.createElement('h1', {}, "Hello H1"),
+    React.createElement('h2', {}, "Hello H2")]
+    )
+)
+
+const heading = React.createElement('h1', {id: 'heading'}, 'Hello From React!');
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// root.render(heading);
+root.render(parent);
