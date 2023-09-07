9906ee4b6def4db4caebb1ec611878bba993ed91
Rajath H R <rajath.nandan33@gmail.com>
2023-09-07 22:30:03 +0530

    Episode 3

--- App.js
+++ App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-{/* <div id='parent'>
-    <div id='child'>
-        <h1>Hello H1</h1>
-        <h2>Hello H2</h2>
+
+const heading = <h1 id='heading'>Hello from JSX 😃 </h1>
+
+
+const SubComponent = () => {
+    return <h2>I'm sub component</h2>
+}
+
+// Composite class - Rendering one component inside another
+const MainComponent = () => {
+    return <div>
+        <h1>I'm main component</h1> 
+        <SubComponent />
+        {SubComponent()}
+        <SubComponent></SubComponent>
+        {heading}
     </div>
-</div> */}
-
-// React create element creates an objects, which while rendering 
-// will convert to html
-const parent = React.createElement('div', {id: 'parent'},
-    React.createElement('div', {id: 'child'},
-    [React.createElement('h1', {}, "Hello H1"),
-    React.createElement('h2', {}, "Hello H2")]
-    )
-)
-
-const heading = React.createElement('h1', {id: 'heading'}, 'Hello From React!');
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// root.render(heading);
-root.render(parent);
+root.render(<MainComponent />);
