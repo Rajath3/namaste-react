8e7831ba0adfce6496a7d4b54f85257f0d434d3d
Rajath H R <rajath.nandan33@gmail.com>
2023-09-11 22:44:55 +0530

    Episode-5 , Intro To useState hook

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,15 +1,25 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/data";
+// Named import, React must be default import
+import {useState}from 'react'
 
 const Body = () => {
+    const [listOfRestaurants, setlistOfRestaurants] = useState(resList);
+
     return (
         <div className='body'>
-            <div className='search-container'>
-                <h5>Search</h5>
+            <div className='filter-container'>
+                <button className="filter-btn" 
+                onClick={() => {
+                    const list = listOfRestaurants.filter(
+                        (res) => res.info.avgRating > 4
+                    )
+                    setlistOfRestaurants(list);
+                }}>Top Restaurant Filter</button>
             </div>
 
             <div className='rest-container'>
-                {resList.map((restaurant) => (
+                {listOfRestaurants.map((restaurant) => (
                      <RestaurantCard key={restaurant.info.id} resData={restaurant.info}/>
                 ))}
                
